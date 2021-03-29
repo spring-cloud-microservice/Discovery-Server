12251df05a9640717693c37bec33c1f5d2a4f043
Saurabh Singh <ss920@gmail.com>
2021-03-29 13:04:12 +0000

    pom dep

--- src/main/java/org/spring/microservice/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/org/spring/microservice/discoveryserver/DiscoveryServerApplication.java
@@ -1,15 +1,38 @@
 package org.spring.microservice.discoveryserver;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+import java.util.List;
+
+@Slf4j
 @EnableEurekaServer
+@SpringBootApplication
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+	@RestController
+	class ServiceInstanceRestController {
+
+		@Autowired
+		private DiscoveryClient discoveryClient;
+
+		@RequestMapping("/service-instances/{applicationName}")
+		public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+			log.info("Getting info {}",applicationName);
+			return this.discoveryClient.getInstances(applicationName);
+		}
 	}
 
 }
