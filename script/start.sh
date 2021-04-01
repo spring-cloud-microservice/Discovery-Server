da41aaf871e487d8a5c4b69697c6d85ae3f48e17
SAURABH SINGH <singhsaurabh920@gmail.com>
2021-04-01 11:00:34 +0000

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -1,6 +1,6 @@
 echo "Starting up discovery server"
 nohup java -jar ../target/discovery-server-0.0.1-SNAPSHOT.jar </dev/null &>/dev/null &
-sleep 2s;
+sleep 10s;
 PROCESS_ID=$(ps -eaf | grep discovery-server-0.0.1-SNAPSHOT.jar | grep -v "grep" | awk '{print $2}')
 echo "Running discovery server: $PROCESS_ID"
 echo "............................................"
